daf87ef9493d87045ab58d409efb0323f0e12c98
RobotikAG1330 <robotikbengymettal@proton.me>
2025-12-17 14:25:37 +0100

    Aktualisiere main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,10 +1,10 @@
 datalogger.onLogFull(function () {
     basic.setLedColors(0xff0000, 0x000000, 0x000000)
-    basic.pause(100)
+    basic.pause(200)
     basic.setLedColors(0x000000, 0xff0000, 0x000000)
     basic.pause(200)
     basic.setLedColors(0x000000, 0x000000, 0xff0000)
-    basic.pause(100)
+    basic.pause(200)
 })
 loops.everyInterval(500, function () {
     datalogger.mirrorToSerial(true)
