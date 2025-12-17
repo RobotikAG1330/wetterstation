8d916b715eb551de3f41a8300d7e4ad36a441212
RobotikAG1330 <robotikbengymettal@proton.me>
2025-12-17 13:54:10 +0100

    Aktualisiere main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,6 +1,21 @@
+datalogger.onLogFull(function () {
+    basic.setLedColors(0xff0000, 0x000000, 0x000000)
+    basic.pause(100)
+    basic.setLedColors(0x000000, 0xff0000, 0x000000)
+    basic.pause(200)
+    basic.setLedColors(0x000000, 0x000000, 0xff0000)
+    basic.pause(100)
+})
 basic.forever(function () {
+	
+})
+loops.everyInterval(100, function () {
     datalogger.mirrorToSerial(true)
     SCD40.start_continuous_measurement()
-    datalogger.log(datalogger.createCV("Temperatur", SCD40.get_temperature(SCD40.SCD40_T_UNIT.C)))
+    datalogger.log(
+    datalogger.createCV("Temperatur", SCD40.get_temperature(SCD40.SCD40_T_UNIT.C)),
+    datalogger.createCV("Relative Luftfeuchtigkeit", SCD40.get_relative_humidity()),
+    datalogger.createCV("", 0)
+    )
     SCD40.stop_continuous_measurement()
 })
